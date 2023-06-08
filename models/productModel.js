df99cf744b6e41073fc6af717c416edf8e89adde
othniel francky <kouamefranckolivier45@gmail.com>
2023-06-08 16:19:22 +0000

    NODE API

---
+++ models/productModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema(
+    {
+        name : {
+            type: String,
+            required: [true, "please entrez le name"]
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: false,
+        },
+        image: {
+            type: String,
+            required: false,
+        }
+
+    },
+    {
+        timestamps: true
+    }
+
+)
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
